36cd9662c670ca7022238463ef6bf0859058916e
unknown <soussiahmed62@gmail.com>
2023-01-23 17:38:47 +0100

    last

--- HygeeAPI/settings.py
+++ HygeeAPI/settings.py
@@ -96,10 +96,10 @@ WSGI_APPLICATION = 'HygeeAPI.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
